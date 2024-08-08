9a5224cde7aa6e4783747859ae5b3582e12e4f31
carrenogf <carrenogf@gmail.com>
2024-08-08 11:52:21 -0300

    vistas sueldos

--- sueldos/urls.py
+++ sueldos/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('home/',views.sueldos_home.as_view(),name='sueldos_home'),
 ]
--- sueldos/views.py
+++ sueldos/views.py
@@ -1,2 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Liquidacion, Empleado
+from empresa.models import Empresa
+from typing import Any
 # Create your views here.
+class sueldos_home(TemplateView):
+	template_name = "sueldos/sueldos_home.html"
+
+	def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+		# Liquidaciones
+		empresa_id = self.request.session['empresa_id']
+		empresa_bbdd = Empresa.objects.get(id=empresa_id)
+		if Liquidacion.objects.filter(empresa=empresa_bbdd).exists():
+			liquidaciones = Liquidacion.objects.filter(empresa=empresa_bbdd).order_by('-periodo')
+			kwargs["liquidaciones"] = liquidaciones[:10]
+   
+		if Empleado.objects.filter(empresa=empresa_bbdd).exists():
+			empleados = Empleado.objects.filter(empresa=empresa_bbdd)
+			kwargs["empleados"] = empleados
+   
+   
+		return super().get_context_data(**kwargs)
